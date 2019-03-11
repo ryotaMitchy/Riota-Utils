101230baacd7458803c1a08cb91bc7e335484d92
r_miyagi <miyagi.tfb@gmail.com>
2019-03-11 22:32:22 +0900

    ファイル取得処理にコメント追加

--- src/main/java/com/scene/riota/App.java
+++ src/main/java/com/scene/riota/App.java
@@ -14,32 +14,36 @@ public class App {
         String path = "/Users/ryota/product/develop/systra/fgp/heroku/tolling";
         FileSearch search = new FileSearch();
 
-        System.out.println("\n●全てのファイルを取得");
-        File[] files = search.listFiles(path, null);
-        printFileList(files);
-        search.clear();
+        // System.out.println("\n●全てのファイルを取得");
+        // File[] files = search.listFiles(path, null);
+        // printFileList(files);
+        // search.clear();
 
         System.out.println("\n●拡張子javaのファイルを取得");
-        files = search.listFiles(path, "*.java");
+        File[] files = search.listFiles(path, "*.java");
         printFileList(files);
         search.clear();
 
-        System.out.println("\n●全てのファイルとディレクトリを取得");
-        files = search.listFiles(path, null, search.TYPE_FILE_OR_DIR, true, 0);
-        printFileList(files);
-        search.clear();
+        // System.out.println("\n●全てのファイルとディレクトリを取得");
+        // files = search.listFiles(path, null, search.TYPE_FILE_OR_DIR, true, 0);
+        // printFileList(files);
+        // search.clear();
 
-        System.out.println("\n●現在の日付から、2日前以降に更新されたファイルを取得");
-        files = search.listFiles(path, null, search.TYPE_FILE, true, 2);
-        printFileList(files);
-        search.clear();
+        // System.out.println("\n●現在の日付から、2日前以降に更新されたファイルを取得");
+        // files = search.listFiles(path, null, search.TYPE_FILE, true, 2);
+        // printFileList(files);
+        // search.clear();
 
-        System.out.println("\n●現在の日付から、30日以前の古いファイルを取得");
-        files = search.listFiles(path, null, search.TYPE_FILE, true, -30);
-        printFileList(files);
-        search.clear();
+        // System.out.println("\n●現在の日付から、30日以前の古いファイルを取得");
+        // files = search.listFiles(path, null, search.TYPE_FILE, true, -30);
+        // printFileList(files);
+        // search.clear();
     }
 
+    /**
+     * 取得したファイルに対して同一の処理を実施する
+     * @param files ファイルリスト
+     */
     private static void printFileList(File[] files) {
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
