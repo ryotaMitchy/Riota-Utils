85188315310f72d56185da26bda4b0a557f2d7f8
ryotaMitchy <miyagi.tfb@gmail.com>
2019-03-19 19:51:11 +0900

    ダウンロード処理の実装

--- static_resource/rest/rest.js
+++ static_resource/rest/rest.js
@@ -1,6 +1,6 @@
 window.onload = function load() {
     console.log('load start');
-    let buttons = $("#restForm").find('input[type="button"]');
+    let buttons = $("#contents").find('input[type="button"]');
     $(buttons).each(function (index, element) {
         // クリックイベントを設定
         $(element).on('click', createRequest);
@@ -13,10 +13,7 @@ window.onload = function load() {
  */
 function createRequest() {
     let form = $(this).closest('form');
-    // form.action = 'http://localhost:5000' + form.action;
-    // form.method = $(this).val();
-    // form.submit();
-    var data = {};
+    let data = {};
     form.find('input[type="text"]').each(function (index, element) {
         // パラメータを集約
         data[element.name] = element.value;
@@ -70,9 +67,58 @@ function fileget(imgfile) {
 
     reader.addEventListener("load", function () {
         $("#base64area").val(reader.result);
+        // download(reader.result);
     }, false);
 
     if (file) {
         reader.readAsDataURL(file);
     }
+}
+
+/**
+ * Base64とMIMEコンテンツタイプからBlobオブジェクトを作成する。
+ * 
+ * @param base64
+ */
+function download(base64) {
+    var mime_ctype = 'application/octet-stream';
+    var blob = toBlob(base64, mime_ctype);
+
+    // if (window.navigator.msSaveBlob) {
+    //     // IEやEdgeの場合、Blob URL Schemeへと変換しなくともダウンロードできる
+    //     window.navigator.msSaveOrOpenBlob(blob, "download.xlsx");
+    // } else {
+        // BlobをBlob URL Schemeへ変換してリンクタグへ埋め込む
+        $("#file_dl").prop("href", window.URL.createObjectURL(blob));
+        // リンクをクリックする
+        document.getElementById("file_dl").click();
+    // }
+}
+
+/**
+ * Base64とMIMEコンテンツタイプからBlobオブジェクトを作成する。
+ * 日本語対応。
+ * 
+ * @param base64 
+ * @param mime_ctype MIMEコンテンツタイプ
+ * @returns Blob
+ */
+function toBlob(base64, mime_ctype) {
+    // 日本語の文字化けに対処するためBOMを作成する。
+    var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
+
+    var bin = atob(base64.replace(/^.*,/, ''));
+    var buffer = new Uint8Array(bin.length);
+    for (var i = 0; i < bin.length; i++) {
+        buffer[i] = bin.charCodeAt(i);
+    }
+    // Blobを作成
+    try {
+        var blob = new Blob([bom, buffer.buffer], {
+            type: mime_ctype,
+        });
+    } catch (e) {
+        return false;
+    }
+    return blob;
 }
